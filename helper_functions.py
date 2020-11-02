314cbac45f10a60f67a4dc4e5526a5c0a0fb7ef3
Radish4649 <69351166+Radish4649@users.noreply.github.com>
2020-11-02 15:01:21 +0900

    Update helper_functions.py
    Changed to a class-based approach for the bot.games object, separating error-handling from the games function itself.

--- helper_functions.py
+++ helper_functions.py
@@ -1,13 +1,31 @@
 import random
 
 
+class Game:
+    def __init__(self, id_, size: int = 6, number: int = 1, kick: bool = False):
+        self.id = id_
+        self.game = random.sample(range(1, size+1), k=number)
+        self.size = size
+        self.number = number
+        self.kick_game = kick
+        self.dead_players = []
+
+    def __str__(self):
+        return "game: {}\nkick game: {}\n dead players: {}".format(
+            self.game, self.kick_game, self.dead_players
+        )
+
+    def shoot(self, shots, user):
+        for current_shot in range(0, shots):
+            self.game = list(map(lambda x: x - 1, self.game))
+            if 0 in self.game:
+                self.dead_players.append(user)
+                self.game = list(filter(lambda x: x != 0, self.game))
+                self.number = self.number - 1
+                return True, current_shot + 1, self.number
+        return False, shots, self.number
+
+
 # this program returns the relevant code for that channel
 def get_code(cxt):
     return str(cxt.guild) + " " + str(cxt.channel)
-
-
-# sets the game settings object to default
-def reset():
-    return {"game": [], "in_progress": 0}
-
-# eventually {"game": [], "in_progress": 0, "kick_mode": 0}
