63088d1db6663d28eab9218b94e3989e278f937b
Radish4649 <69351166+Radish4649@users.noreply.github.com>
2020-11-13 14:26:33 +0900

    Create game_class.py
    moved the game class to it's own file

---
+++ game_class.py
@@ -0,0 +1,26 @@
+import random
+
+
+class Game:
+    def __init__(self, id_, size: int = 6, number: int = 1, kick: bool = False):
+        self.id = id_
+        self.game = random.sample(range(1, size+1), k=number)
+        self.size = size
+        self.number = number
+        self.kick_game = kick
+        self.dead_players = []
+
+    def __str__(self):
+        return "game: {}\nkick game: {}\ndead players: {}".format(
+            self.game, self.kick_game, self.dead_players
+        )
+
+    def shoot(self, shots, user):
+        for current_shot in range(0, shots):
+            self.game = list(map(lambda x: x - 1, self.game))
+            if 0 in self.game:
+                self.dead_players.append(user)
+                self.game = list(filter(lambda x: x != 0, self.game))
+                self.number = self.number - 1
+                return True, current_shot + 1, self.number, self.kick_game
+        return False, shots, self.number, self.kick_game
